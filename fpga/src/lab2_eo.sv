d5ab2218ce7858a72633660cdf18833a44e36afb
eoinoconnell04 <eoinoconnell04@gmail.com>
2025-09-03 17:48:34 -0700

    Initial Versions of verilog files, plus files copied from lab1

--- fpga/src/lab2_eo.sv
+++ fpga/src/lab2_eo.sv
@@ -7,6 +7,7 @@ Module Function: This is the top level module for E155 Lab 2. It performs 2 main
 2. Controls 5 led lights that display the binary sum of the two hex digits.
 */
 module lab2_eo(
+	input  logic reset,
 	input  logic [3:0] s1,
 	input  logic [3:0] s2,
 	output logic [6:0] seg1,
@@ -15,16 +16,26 @@ module lab2_eo(
 );
 
 	logic clk, divided_clk;
+	logic [3:0] display_input;
+	logic [6:0] display_output;
 	
-	// Internal high-speed oscillator
+	// Internal high-speed oscillator (outputs 48 Mhz clk)
 	HSOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(clk));
 
-    // Initialize clock divider (outputs 2.4 Hz)
-    divider div (.clk(clk), .divided_clk(divided_clk));
+    // Initialize clock divider (Divides by 4800, dividing 48 MHz to 10 kHz)
+    divider #(.TOGGLE_COUNT(2400)) div	(.clk(clk), .reset(reset), .divided_clk(divided_clk));
 
-    // Initialize seven segment display
-    display dis (.s(s), .seg(seg));
+	// Seven segment display Input Mux
+	assign display_input = divided_clk ? s1 : s2;
 
-    assign led = {divided_clk, s[3] & s[2], s[1] ^ s[0]};
+	// Seven segment display Output Demux
+	assign seg1 = divided_clk ? display_output : 7'b1111111;
+	assign seg2 = divided_clk ? 7'b1111111 : display_output;
+
+    // Initialize single seven segment display
+    display dis (.s(display_input), .seg(display_output));
+
+	// Assign the 5 LEDs to the sum of the 4-bit hex digits
+    assign led = s1 + s2;
 
 endmodule
