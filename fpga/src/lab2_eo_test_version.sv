baf5e3853fdb3629b6b7de6b282e5348885aa1d6
eoinoconnell04 <eoinoconnell04@gmail.com>
2025-09-05 17:02:53 -0700

    Changed test version to not divide clock at all

--- fpga/src/lab2_eo_test_version.sv
+++ fpga/src/lab2_eo_test_version.sv
@@ -15,21 +15,21 @@ module lab2_eo_test_version(
 	output logic [4:0] led
 );
 
-	logic divided_clk;
+	//logic divided_clk;
 	logic [3:0] display_input;
 	
 	// Internal high-speed oscillator (outputs 48 Mhz clk)
 	//HSOSC hf_osc (.CLKHFPU(1'b1), .CLKHFEN(1'b1), .CLKHF(clk));
 
     // Initialize clock divider (Divides by 4800, dividing 48 MHz to 10 kHz)
-    divider #(.TOGGLE_COUNT(1)) div	(.clk(clk), .reset(reset), .divided_clk(divided_clk));
+    //divider #(.TOGGLE_COUNT(1)) div	(.clk(clk), .reset(reset), .divided_clk(divided_clk));
 
 	// Seven segment display Input Mux (if divided_clk is high, then s1 selected. If divided_clk is low then s2 selected)
-	assign display_input = divided_clk ? s1 : s2;
+	assign display_input = clk ? s1 : s2;
 
 	// Choose which seven segment display to power.
-	assign display1 = divided_clk;
-	assign display2 = ~divided_clk;
+	assign display1 = clk;
+	assign display2 = ~clk;
 
     // Initialize single seven segment display
     display dis (.s(display_input), .seg(seg));
